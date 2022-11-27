d0e18fc868ae41bfcc213f0b7eef3c63413452d4
baragiolaj <baragiolaj@gmail.com>
2022-11-27 17:08:30 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,20 @@
+for (let i = 1; i = 2; i) {
+    let numedad = parseInt(prompt("ingrese su edad"));
+    if (numedad < 18){
+    alert("no puede constinuar con la encuetras llama a un adulto")
+    i=1
+} else {
+    alert ("puedes continuar");
+    i = 2;
+    break
+}}
+
+let producto1 = parseInt (prompt("ingrese valor del producto 1"))
+let producto2 = parseInt (prompt("ingrese valor del producto 2"))
+let producto3 = parseInt (prompt("ingrese valor del producto 3"))
+
+alert (resultado = producto1+producto2+producto3)
+
+if (resultado >= 300) {
+    alert ("usted tiene un 20% de decuento, usted paga " + resultado * 0.8)
+}
